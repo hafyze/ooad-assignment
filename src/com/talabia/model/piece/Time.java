25d1346c10aeba8e5689cc2abb3376dadefad57c
hafyze <hafizmatqwer@gmail.com>
2024-01-20 22:00:59 +0800

    update: 'time' can move

--- src/com/talabia/model/piece/Time.java
+++ src/com/talabia/model/piece/Time.java
@@ -4,7 +4,8 @@ import com.talabia.model.board.Square;
 
 import java.util.ArrayList;
 
-public class Time extends AbstractPiece{
+public class Time extends AbstractPiece {
+
     public Time(PieceColor pieceColor) {
         super("Time", pieceColor);
         this.pieceImageName = pieceColor.toString() + "_" + pieceName;
@@ -12,11 +13,33 @@ public class Time extends AbstractPiece{
 
     @Override
     public void setPossibleMoves(Square currentSquare, Square[][] squares) {
+        ArrayList<Square> possibleMoves = new ArrayList<>();
 
+        int currentRow = currentSquare.getRow();
+        int currentCol = currentSquare.getColumn();
+
+        // Move diagonal logic
+        for (int rowDirection : new int[]{-1, 1}) {
+            for (int colDirection : new int[]{-1, 1}) {
+                int row = currentRow + rowDirection;
+                int col = currentCol + colDirection;
+
+                while (row >= 0 && row < squares.length && col >= 0 && col < squares[0].length) {
+                    Square square = squares[row][col];
+                    possibleMoves.add(square);
+
+                    // Diagonal movement
+                    row += rowDirection;
+                    col += colDirection;
+                }
+            }
+        }
+
+        this.possibleMoves = possibleMoves;
     }
 
     @Override
     public ArrayList<Square> getPossibleMoves() {
-        return null;
+        return this.possibleMoves;
     }
 }
