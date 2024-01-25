1ec220b2fc87a7aedc5b7c77b88ad35a9bc4e517
hafyze <hafizmatqwer@gmail.com>
2024-01-25 20:30:24 +0800

    update: code refactor for setPossibleMove in Plus piece

--- src/com/talabia/model/piece/Plus.java
+++ src/com/talabia/model/piece/Plus.java
@@ -24,49 +24,36 @@ public class Plus extends AbstractPiece{
         int currentCol = currentSquare.getColumn();
 
         // Move up
-        boolean canMove = true;
-        for (int i = 1; i <= boardRow && canMove; i++) {
-            int newRow = currentRow - i;
-            if (newRow >= 0) {
-                //System.out.println("Move up");
-                possibleMoves.add(squares[newRow][currentCol]);
-                canMove = !squares[newRow][currentCol].isOccupied();
-            }
-        }
+        calculateMoves(possibleMoves, squares, currentRow, currentCol, -1, 0, boardRow);
 
         // Move down
-        canMove = true;
-        for (int i = 1; i <= boardRow && canMove; i++) {
-            int newRow = currentRow + i;
-            if (newRow < boardRow) {
-                //System.out.println("Move down");
-                possibleMoves.add(squares[newRow][currentCol]);
-                canMove = !squares[newRow][currentCol].isOccupied();
-            }
-        }
+        calculateMoves(possibleMoves, squares, currentRow, currentCol, 1, 0, boardRow);
 
-        // Move horizontally
-        canMove = true;
-        for (int i = 1; i <= boardCol && canMove; i++) {
-            int newCol = currentCol - i;
-            if (newCol >= 0) {
-                possibleMoves.add(squares[currentRow][newCol]);
-                //System.out.println("Move left");
-                canMove = !squares[currentRow][newCol].isOccupied();
-            }
-        }
-        canMove = true;
-        for (int i = 1; i <= boardCol && canMove; i++) {
-            int newCol = currentCol + i;
-            if (newCol < boardCol) {
-                possibleMoves.add(squares[currentRow][newCol]);
-                //System.out.println("Move right");
-                canMove = !squares[currentRow][newCol].isOccupied();
-            }
-        }
+        // Move left
+        calculateMoves(possibleMoves, squares, currentRow, currentCol, 0, -1, boardCol);
+
+        // Move right
+        calculateMoves(possibleMoves, squares, currentRow, currentCol, 0, 1, boardCol);
+
         this.possibleMoves = possibleMoves;
     }
 
+    private void calculateMoves(ArrayList<Square> possibleMoves, Square[][] squares,
+                                int currentRow, int currentCol, int rowDirection,
+                                int colDirection, int maxSteps) {
+        boolean canMove = true;
+        for (int i = 1; i <= maxSteps && canMove; i++) {
+            int newRow = currentRow + i * rowDirection;
+            int newCol = currentCol + i * colDirection;
+
+            if (newRow >= 0 && newRow < squares.length && newCol >= 0 && newCol < squares[0].length) {
+                possibleMoves.add(squares[newRow][newCol]);
+                canMove = !squares[newRow][newCol].isOccupied();
+            } else {
+                break;
+            }
+        }
+    }
     @Override
     public ArrayList<Square> getPossibleMoves() {
         return this.possibleMoves;
